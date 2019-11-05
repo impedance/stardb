ca638a829fa5fac814011dba6bc86339e4875278
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2019-11-05 18:32:29 +0300

    creating stardb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
+class SwapiService {
 
-const getResource = async (url) => {
-    const res = await fetch(url);
-    return await res.json();
-};
+    _apiBase = 'https://swapi.co/api';
 
-getResource('https://swapi.co/api/people/1/')
-    .then((body) => {
-        console.log(body)
-    });
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+        }
+        return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`)
+        return res.results;
+    }
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`)
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`)
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}/`)
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`)
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}/`)
+    }
+}
+
+const swapi = new SwapiService();
+
+swapi.getStarship(3).then((s) => {
+    console.log(s.name);
+})
+
